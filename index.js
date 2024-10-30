56e8e43f747358157bb9cc9b050b460d1805d24c
Jevin Fernando <jevin.fernando@electrolux.com>
2024-10-30 21:04:36 +0100

    7

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
 
-const corsOptions = {
-  origin: ['https://fsm-three.vercel.app'],
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-// app.use(cors())
+app.use(cors())
 
 app.use(createProxyMiddleware({
   router: (req) => new URL(req.path.substring(1)),
@@ -19,7 +14,7 @@ app.use(createProxyMiddleware({
 }))
 
 app.listen(8088, () => {
-  console.info('proxy server is running on port 8088')
+  console.info('Proxy server is running on port 8088')
 })
 
 module.exports = app
